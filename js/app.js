67c9aa3f8d3fc536be1b9f1d512def573b9e98cc
Max <lutsenko.maxim@gmail.com>
2017-02-22 14:49:05 +0200

    with  ajax

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
+app.post('/Business Theme/js/app.js',  function (req, res) {
+   cosole.log(req.body);
 
+    return res.json({a: 1});
+});
--- js/main_js.js
+++ js/main_js.js
@@ -9,13 +9,30 @@ $(document).ready(function() {
        }
     });
 //AJAX form submission
+$('.top-form').on('submit', function (e) {
+        e.preventDefault();
+    let name = $('.field-name').val().trim(),
+        email = $('.field-email').val().trim(),
+        phone = $('.field-phone').val().trim();
+    let data = {
+        name: name,
+        email: email,
+        phone: phone
+    };
 
+    $.ajax({
+        url: '/Business Theme/js/app.js',
+        method: 'post',
+        data: JSON.stringify(data),
+        // headers: {
+        //     'content-type': 'application/json'
+        // },
+        success: function (data) {
+            console.log(data);
+        }
 
-
-
-
-
-
+    })
+    });
 //AJAX form submission END
 
 
